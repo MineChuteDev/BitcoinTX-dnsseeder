151b86a6f4382ea211962129ee8897a20bb066d4
MineChuteDev <mcdev@minechute.com>
2015-11-08 22:48:37 -0500

    Initial updates for BTX after fork from Dash

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ public:
   CDnsSeedOpts() : nThreads(96), nDnsThreads(4), nPort(53), mbox(NULL), ns(NULL), host(NULL), tor(NULL), fUseTestNet(false), fWipeBan(false), fWipeIgnore(false) {}
 
   void ParseCommandLine(int argc, char **argv) {
-    static const char *help = "Darkcoin-seeder\n"
+    static const char *help = "BitcoinTX-seeder\n"
                               "Usage: %s -h <host> -n <ns> [-m <mbox>] [-t <threads>] [-p <port>]\n"
                               "\n"
                               "Options:\n"
@@ -342,13 +342,13 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"static-dnsseed.darkcoin.io", "static-dnsseed.darkcoin.qa", ""};
-static const string testnet_seeds[] = {"static-testnet-seed.darkcoin.io", "static-testnet-seed.darkcoin.qa", ""};
+static const string mainnet_seeds[] = {"45.55.92.248", "45.55.175.208", "45.55.179.167", ""};  
+static const string testnet_seeds[] = {"45.55.92.248", "45.55.175.208", "45.55.179.167", ""}; 
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
   if (!fTestNet){
-    db.Add(CService("darkcoin.io", 9999), true);
+    db.Add(CService("45.55.92.248", 28280), true);
   }
   do {
     for (int i=0; seeds[i] != ""; i++) {
@@ -377,10 +377,10 @@ int main(int argc, char **argv) {
   bool fDNS = true;
   if (opts.fUseTestNet) {
       printf("Using testnet.\n");
-      pchMessageStart[0] = 0xce;
-      pchMessageStart[1] = 0xe2;
-      pchMessageStart[2] = 0xca;
-      pchMessageStart[3] = 0xff;
+      pchMessageStart[0] = 0xb1;
+      pchMessageStart[1] = 0xb5;
+      pchMessageStart[2] = 0xa2;
+      pchMessageStart[3] = 0xa4;
       seeds = testnet_seeds;
       fTestNet = true;
   }
