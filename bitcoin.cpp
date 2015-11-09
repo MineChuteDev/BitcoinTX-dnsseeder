0dedf764e18709cc322a2ec53efad0e4000bbed3
MineChuteDev <mcdev@minechute.com>
2015-11-08 22:46:54 -0500

    Initial updates for BTX after fork from Dash

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/darkcoin-seeder:0.1.2/";
+    string ver = "/bitcointx-seeder:0.2.15.0/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
@@ -296,7 +296,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
 
 /*
 int main(void) {
-  CService ip("darkcoin.io", 9999, true);
+  CService ip("bitcointx.info", 28280, true);
   vector<CAddress> vAddr;
   vAddr.clear();
   int ban = 0;
